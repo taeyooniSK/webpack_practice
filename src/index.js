2f0d0156d6583ddddd09b29199fc54bfb41e88be
unknown <yeotaeyoon@gmail.com>
2019-07-22 00:24:52 +0900

    Asset management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import _ from "lodash";
+import './style.css';
+import Spooky from "./spooky.png";
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add("hello");
   
+    // Add the image to my exisitng div.
+    const myIcon = new Image();
+    myIcon.src = Spooky;
+
+    element.appendChild(myIcon);
+
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,45 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {                        // webpack uses a regular expression to determine which files it should look for and serve to a specific loader. 
+                                     // In this case any file that ends with .css will be served to the style-loader and the css-loader.
+                test: /\.css$/,      
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                    {                                   //https://github.com/tcoopman/image-webpack-loader
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                              progressive: true,
+                              quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                              enabled: false,
+                            },
+                            pngquant: {
+                              quality: '65-90',
+                              speed: 4
+                            },
+                            gifsicle: {
+                              interlaced: false,
+                            }
+                        }
+                    }
+                ]
+            },
+        ]
     }
 }
